e0e1b0539473939ee887dba3b8056a4656dee018
benj <benj@knowbe4.com>
2019-06-28 15:56:44 -0400

    getting random teams

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,98 @@
 <template lang="pug">
-  h1 this is the home page
+  .home-container
+    h1 Team Name Generator
+    form(@submit.prevent="onSubmit" v-if="readyToEnter")
+      .input-group
+        label(for="player-one") Player 1
+        input(type="text" id="player-one" v-model="players[0]")
+      .input-group
+        label(for="player-two") Player 2
+        input(type="text" id="player-two" v-model="players[1]")
+      .input-group
+        label(for="player-three") Player 3
+        input(type="text" id="player-three" v-model="players[2]")
+      .input-group
+        label(for="player-four") Player 4
+        input(type="text" id="player-four" v-model="players[3]")
+      button(type="submit") Generate Team Names
+      p {{ teamOne }}
+      p {{ teamTwo }}
 </template>
-
 
 <script>
 export default {
-  name: 'home'
+  name: 'home',
+  data () {
+    return {
+      players: [],
+      readyToEnter: true,
+      teamOne: '',
+      teamTwo: ''
+    }
+  },
+  methods: {
+    shuffle (array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+      return array
+    },
+    onSubmit () {
+      this.shuffle(this.players)
+      this.teamOne = this.players.slice(0,2)
+      this.teamTwo = this.players.slice(2,4)
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.home-container {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+
+h1 {
+  margin: 5px;
+}
+
+form {
+  min-width: 310px;
+  max-width: 375px;
+  margin: 10px;
+}
+
+.input-group {
+  margin: 10px 0;
+  display: flex;
+  flex-direction: column;
+}
+
+input {
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  font-size: 12px;
+  padding: 5px 0;
+  text-indent: 5px;
+}
+
+button {
+  margin-top: 20px;
+  width: 100%;
+  font-size: 14px;
+  padding: 7px 10px;
+  border: 1px solid #b200ff;
+  background-color: #b200ff;
+  color: #fff;
+  border-radius: 3px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #490068;
+  border: 1px solid #490068;
+}
+</style>
+
